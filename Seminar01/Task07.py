e5e8d5bae261560112efa34e80fd8006386aea6b
Vitaliy <vitaliy.klimov@mail.ru>
2022-12-21 22:24:01 +0300

    Task08 complete

--- Seminar01/Task07.py
+++ Seminar01/Task07.py
@@ -4,4 +4,4 @@
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            print((not (x or y or z)) == (not x and not y and not z), end="_")
+            print((not (x or y or z)) == (not x and not y and not z), end=", ")
