ac347392b1ba8e327e0b02bc2ac029c36f56ec5e
Vitaliy <vitaliy.klimov@mail.ru>
2022-12-30 00:15:52 +0300

    Task18 complete

--- Seminar02/Task18.py
+++ Seminar02/Task18.py
@@ -1,2 +1,11 @@
 # Реализуйте алгоритм перемешивания списка.
 
+import random 
+ 
+rndList = []
+for i in range(10):
+    rndList.append(random.randint(0, 20)) 
+print(rndList)
+ 
+random.shuffle(rndList)
+print(rndList)
