b502e0bc38a53f4c326084774f6820efd1a01782
Vitaliy <vitaliy.klimov@mail.ru>
2022-12-30 00:14:19 +0300

    Task17 complete

--- Seminar02/Task17.py
+++ Seminar02/Task17.py
@@ -2,3 +2,12 @@
 # Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
+import random 
+n = int(input("Введите число N: "))
+rndList = []
+for i in range(n):
+    rndList.append(random.randint(-n, n)) 
+print(rndList)
+ 
+a = open("file.txt")
+print(rndList[int(a.readline())] * rndList[int(a.readline(2))])
