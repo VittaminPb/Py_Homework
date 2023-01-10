cdea33f22baa44a7f811eca80dbb338275811e3d
Vitaliy <vitaliy.klimov@mail.ru>
2023-01-10 20:31:25 +0300

    Task32 complete

--- Seminar04/Task32.py
+++ Seminar04/Task32.py
@@ -1,2 +1,9 @@
 # 32. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
+from random import randint
+
+list = [randint(0, 10) for i in range(randint(10, 15))]
+print(list)
+
+non_repet = [i for i in list if list.count(i)==1]
+print(non_repet)
