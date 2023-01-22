2e18448fa29de45e445cacec61251a16c38050cf
Vitaliy <vitaliy.klimov@mail.ru>
2023-01-22 23:31:16 +0300

    Task46 complete

--- Seminar06/Task46.py
+++ Seminar06/Task46.py
@@ -3,3 +3,15 @@
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
 
+def degree(num, deg):
+    if (deg < 0):
+        return "Я так еще не умею ))"
+    if (deg == 1):
+        return (num)
+    if (deg != 1):
+        return (num * degree(num, deg - 1))
+
+num = int(input("Введите число: "))
+deg = int(input("Введите степень: "))
+
+print(f"A = {num}, B = {deg} ->", degree(num, deg))
